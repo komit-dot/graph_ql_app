04b710d7adec90f9d1264fe8e79623e2b6653ca2
komit-dot <komitcha@gmail.com>
2024-04-04 17:17:28 +0530

    integration of custom validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
-	has_many :posts
+  has_many :posts
+  #validates_uniqueness_of :add_name
+  validate :full_name_uniqueness
+
+	def full_name_uniqueness
+    if User.exists?(first_name: first_name, last_name: last_name)
+      errors.add :base, "#{add_name} already exists"
+    end
+	end
+
+ private
+
+  def add_name
+   "#{first_name} #{ last_name}"
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_24_130531) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_04_110116) do
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title"
@@ -21,10 +21,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_24_130531) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name"
   end
 
   add_foreign_key "posts", "users"
